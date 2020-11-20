d5e2c07b984594e7f077e390ca5e2e1467f680b3
Opilane TTHK <opilane@tthk.ee>
2020-11-20 10:09:25 +0200

    Create Web Page

---
+++ FridayWorkWithFiles/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+
+namespace FridayWorkWithFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DeletetoiletPaper();
+            Deletecheese();
+        }
+        public static void DeletetoiletPaper()
+        {
+            string rootPath = @"C:\Users\opilane\samples\töö\fruit";
+            string fileName = "toiletPaper.txt";
+
+            if (File.Exists(Path.Combine(rootPath, fileName)))
+            {
+                File.Delete(Path.Combine(rootPath, fileName));
+                Console.WriteLine("File deleted");
+            }
+            else
+            {
+                Console.WriteLine("File not found");
+            }
+        }
+        public static void Deletecheese()
+        {
+            string rootPath = @"C:\Users\opilane\samples\töö\vegetables";
+            string fileName = "cheese.txt";
+
+            if (File.Exists(Path.Combine(rootPath, fileName)))
+            {
+                File.Delete(Path.Combine(rootPath, fileName));
+                Console.WriteLine("File deleted");
+            }
+            else
+            {
+                Console.WriteLine("File not found");
+            }
+        }
+    }
+}
+
---
+++ FridayWorkWithFiles2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+
+namespace FridayWorkWithFiles2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the file name (include extensions):");
+            string userInput = Console.ReadLine();
+            MoveOnionsfile();
+        }
+     
+        public static void MoveOnionsfile()
+        {
+            string rootPath = @"C:\Users\opilane\samples\töö";
+            string destinationDirectory = @"C:\Users\opilane\samples\töö\vegetables";
+            string fileName = "onions.txt";
+
+            File.Move(Path.Combine(rootPath, fileName), Path.Combine(destinationDirectory, fileName));
+        }
+    }
+}
+
